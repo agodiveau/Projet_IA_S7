39e3053a61a5d63d6d0e4a2afb0e2313fdec0fb8
Andréa <andrea.godiveau@gmail.com>
2025-01-08 10:50:53 +0100

    MàJ BBN

--- Code/scripts/bayesian_neural_networks.py
+++ Code/scripts/bayesian_neural_networks.py
@@ -1,78 +1,81 @@
+import numpy as np
 import tensorflow as tf
-import tensorflow_probability as tfp
-import tensorflow_datasets as tfds
+import cv2
+from Composants_connexes import extract_components, load_luminaire_features  # Importer les fonctions du fichier composants_connexes
+from kppv import knn_classification
 
-# Chargement du dataset
-(ds_train, ds_test), ds_info = tfds.load(
-    'fashion_mnist',
-    split=['train', 'test'],
-    as_supervised=True,
-    with_info=True
-)
+def get_number_classes(luminaire_features) :
+    return len(luminaire_features)
 
-def preprocess(image, label):
-    image = tf.cast(image, tf.float32) / 255.0
-    return image, label
+def get_number_components(components) :
+    return len(components)
 
-ds_train = ds_train.map(preprocess).batch(128).shuffle(1000)
-ds_test = ds_test.map(preprocess).batch(128)
+def initialize_unconditional_probabilities(number_classes) :
+    probability_belonging_to_Cj = []
+    for i in range(number_classes):
+        probability_belonging_to_Cj.append(1/number_classes)
+    return probability_belonging_to_Cj
 
-tfd = tfp.distributions
 
-def prior_fn(kernel_size, bias_size, dtype=None):
-    n = kernel_size + bias_size
-    prior_model = tf.keras.Sequential([
-        tfp.layers.DistributionLambda(lambda t: tfd.MultivariateNormalDiag(
-            loc=tf.zeros(n), scale_diag=tf.ones(n)))
-    ])
-    return prior_model
+# Calcul des distances entre chaque composant et chaque luminaire
+def calculate_distances_components_luminaires(components, luminaire_features) :
+    components_tensor = tf.convert_to_tensor(components, dtype=tf.float32)
+    luminaires_tensor = tf.convert_to_tensor(luminaire_features, dtype=tf.float32)
+    distances = tf.norm(components_tensor[:, None, :] - luminaires_tensor[None, :, :], axis=-1)
+    return distances
 
-def posterior_fn(kernel_size, bias_size, dtype=None):
-    n = kernel_size + bias_size
-    posterior_model = tf.keras.Sequential([
-        tfp.layers.VariableLayer(
-            params=2 * n,
-            dtype=dtype,
-            initializer=tf.keras.initializers.RandomNormal(mean=0., stddev=0.1)
-        ),
-        tfp.layers.DistributionLambda(lambda t: tfd.MultivariateNormalDiag(
-            loc=t[..., :n],
-            scale_diag=tf.nn.softplus(t[..., n:])
-        ))
-    ])
-    return posterior_model
+def get_class_index(label) :
+    if label == "Luminaire 01" :
+        return 0
+    elif label == "Luminaire 03" :
+        return 1
+    elif label == "Luminaire 07" :
+        return 2
+    elif label == "Luminaire 10" :
+        return 3
+    elif label == "Luminaire 11" :
+        return 4
+    elif label == "Luminaire 15" :
+        return 5
 
-# Modèle fonctionnel
-inputs = tf.keras.Input(shape=(28, 28))  # Définir l'entrée
-x = tf.keras.layers.Flatten()(inputs)    # Transformation en tenseur plat
 
-# Première couche bayésienne
-x = tfp.layers.DenseVariational(
-    units=128,
-    make_prior_fn=prior_fn,
-    make_posterior_fn=posterior_fn,
-    kl_weight=1/ds_info.splits['train'].num_examples,
-    activation='relu'
-)(x)
+#Charger l'image du plan.
+plan_path = "../data/plans/plan.png"
+plan_img = cv2.imread(plan_path)
 
-# Deuxième couche bayésienne
-outputs = tfp.layers.DenseVariational(
-    units=10,
-    make_prior_fn=prior_fn,
-    make_posterior_fn=posterior_fn,
-    kl_weight=1/ds_info.splits['train'].num_examples,
-    activation='softmax'
-)(x)
+components, _ = extract_components(plan_img) #Récupérer les caractéristiques des composantes connexes détectées dans l'image.
+luminaire_features, luminaire_labels, _ = load_luminaire_features() #Récupérer les caractéristiques des luminaires du catalogue.
 
-model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-model.compile(
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-    metrics=['accuracy']
-)
+#Phase 1 : Construction du réseau bayésien.
 
-# Entraînement et Évaluation
-model.fit(ds_train, epochs=10, validation_data=ds_test)
-loss, accuracy = model.evaluate(ds_test)
-print(f"Test Loss: {loss:.4f}, Test Accuracy: {accuracy:.4f}")
+number_classes = get_number_classes(luminaire_features)
+number_components = get_number_components(components)
+
+print("Il y a", number_classes, "classes.")
+print(number_components, "composantes connexes ont été détectées dans l'image.")
+
+#Initialisation des probabilités inconditionnelles ou P(C_{j}).
+proba_Cj = initialize_unconditional_probabilities(number_classes)
+
+print("P(Cj) initiales :", proba_Cj)
+
+#Phase 1 : Apprentissage pour déterminer les probabilités conditionnelles P(X_{i} | C_{j})
+
+K = 3
+labels_kppv = knn_classification(components, luminaire_features, luminaire_labels, K)
+
+proba_Xi_and_Cj = [[0]*len(luminaire_features[0]) for i in range(len(luminaire_features))] # matrice de b lignes et m colonnes
+
+seuils = [0.4, 0.1, 0.5, 0.07, 0.07, 0.0001]
+
+for i in range(len(components)) :
+    class_index = get_class_index(labels_kppv[i])
+
+    for j in range(len(components[i])) :
+        if (components[i][j] < seuils[j]) and (class_index != None) :
+            proba_Xi_and_Cj[class_index][j] += 1
+
+print("P(Xi et Cj) =", proba_Xi_and_Cj)
+
+
